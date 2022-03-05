9a122f79befd99e0027d941299466a5a113c0135
Alex <dinarwijaksono11@gmail.com>
2022-03-05 19:12:00 +0700

    desain ui management contanct

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -19,7 +19,8 @@
     </nav>
 
     <main class=" grid grid-cols-8 h-full gap-2 p-1">
-        <aside class="bg-white overflow-y-auto p-2 col-span-2" style="height: 505px">
+
+        <aside class="bg-white relative overflow-y-auto p-2 col-span-2" style="height: 505px">
 
             <div class="border mb-1 border-green-500 p-2 rounded">
                 <h1>damayanti</h1>
@@ -28,6 +29,55 @@
             <div class="border mb-1 hover:bg-blue-300 border-green-500 p-2 rounded">
                 <h1>Pirda</h1>
             </div>
+
+            <!-- list kontak -->
+            <section id="listKontak"
+                class="bg-blue-300 hidden mb-2 absolute top-3 left-3 right-3 shadow-lg rounded h-52 overflow-y-auto p-2">
+
+                <a id="xListContact">
+                    <div class="bg-red-500 text-center rounded p-1 mb-1 text-white">Tutup box</div>
+                </a>
+
+                <a id="tAddContact">
+                    <div class="bg-orange-500 text-center rounded p-1 mb-1 text-white">Tambah kontak</div>
+                </a>
+
+                <a href="">
+                    <div class="bg-white rounded p-1 mb-1 text-blue-500">damayanti</div>
+                </a>
+
+                <a href="">
+                    <div class="bg-white rounded p-1 mb-1 text-blue-500">damayanti</div>
+                </a>
+
+                <a href="">
+                    <div class="bg-white rounded p-1 mb-1 text-blue-500">damayanti</div>
+                </a>
+            </section>
+            <!-- end list kontak -->
+
+            <!-- tambah kontak -->
+            <section id="boxAddContact"
+                class="bg-blue-300 hidden absolute top-3 left-3 right-3 shadow-lg rounded overflow-y-auto p-2">
+
+                <a id="xBoxAddContact">
+                    <div class="bg-red-500 text-center rounded p-1 mb-1 text-white">Tutup box</div>
+                </a>
+
+                <form action="" method="post">
+                    @csrf
+                    <div class="mb-2">
+                        <input type="email" class="w-full border-2 p-1 px-2 border-orange-500" placeholder="Email">
+                    </div>
+
+                    <button type="submit"
+                        class="bg-orange-500 text-white w-full p-2 rounded text-center">Tambah</button>
+                </form>
+
+            </section>
+            <!-- end tambah kontak -->
+
+            <a id="tChat" class="absolute bottom-5 right-5 bg-orange-500 text-center p-2 text-white rounded">Chat</a>
 
         </aside>
 
@@ -71,6 +121,33 @@
         </article>
 
     </main>
+
+    <script>
+    let listKontak = document.getElementById('listKontak');
+    let tChat = document.getElementById('tChat');
+    let tAddContact = document.getElementById('tAddContact');
+    let boxAddContact = document.getElementById('boxAddContact');
+    let xListContact = document.getElementById('xListContact');
+    let xBoxAddContact = document.getElementById('xBoxAddContact');
+
+
+    tChat.addEventListener('click', function() {
+        listKontak.classList.toggle('hidden')
+    })
+
+    tAddContact.addEventListener('click', function() {
+        listKontak.classList.toggle('hidden')
+        boxAddContact.classList.toggle('hidden')
+    })
+
+    xListContact.addEventListener('click', function() {
+        listKontak.classList.toggle('hidden')
+    })
+
+    xBoxAddContact.addEventListener('click', function() {
+        boxAddContact.classList.toggle('hidden');
+    })
+    </script>
 
 </body>
 
